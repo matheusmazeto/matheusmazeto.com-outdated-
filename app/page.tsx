1b1e5d37d671e4bc771e7a875e41b49b9f1fcfe1
Matheus Mazeto <nb27582@novabase.pt>
2023-04-26 14:43:49 +0100

    feat: posts page

--- app/page.tsx
+++ app/page.tsx
@@ -7,32 +7,20 @@ export default function Home() {
   const posts = getAllPosts(['slug', 'title', 'date', 'language'])
 
   return (
-    <main>
-      <article className="mt-8">
-        {posts.map((post) => (
-          <article
-            key={post.slug}
-            className="border-t border-slate-200 py-4 dark:border-slate-800"
+    <main className="w-full max-w-prose py-6">
+      {posts.map((post) => (
+        <article key={post.slug} className="py-3 flex items-center">
+          <time
+            dateTime={post.date}
+            className="min-w-[160px] text-stone-500 font-light text-left"
           >
-            <h2 className="text-2xl font-bold dark:text-slate-100">
-              <Link href={`/blog/${post.slug}`}>{post.title}</Link>
-            </h2>
-            <div className="mt-1">
-              <time dateTime={post.date} className="text-sm text-slate-400">
-                {format(new Date(post.date), 'yyyy MMM dd')}
-              </time>
-            </div>
-            <div className="mt-4">
-              <Link
-                href={`/blog/${post.slug}`}
-                className="text-blue-500 hover:text-blue-400"
-              >
-                Read Post
-              </Link>
-            </div>
-          </article>
-        ))}
-      </article>
+            {format(new Date(post.date), 'MMMM dd, yyyy')}
+          </time>
+          <h2 className="font-bold text-[#0c00ff]">
+            <Link href={`/blog/${post.slug}`}>{post.title}</Link>
+          </h2>
+        </article>
+      ))}
     </main>
   )
 }
